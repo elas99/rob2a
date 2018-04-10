466f922d49d3de16d6680effd0c7f6ade7e618f0
elas99 <elas99@hotmail.com>
2018-04-10 10:50:41 +0000

    Add files via upload

--- Functions/functions.c
+++ Functions/functions.c
@@ -1,3 +1,4 @@
+int threshold = 2200;
 void Controller()
 {
 	int running = 1;
@@ -39,6 +40,10 @@ void stopMotors()
 	motor[rightMotor] = 0;
 	motor[leftMotor]  = 0;
 }
+void Forward(float r);
+void TurnLeft(float r);
+void TurnRight(float r);
+
 const float rotations = 180.0;
 void Forward(float r)
 {
@@ -50,6 +55,35 @@ void Forward(float r)
   {
     motor[rightMotor] = 63;         /* Run both motors        */
     motor[leftMotor]  = 63;         /* forward at half speed. */
+  }
+  motor[rightMotor] = 0;            /* Stop both motors!  This is important so that each function          */
+  motor[leftMotor]  = 0;            /* can act independantly as a "chunk" of code, without any loose ends. */
+}
+void TurnLeft(float r)
+{
+  SensorValue[rightEncoder] = 0;    /* Clear the encoders for    */
+  SensorValue[leftEncoder]  = 0;    /* consistancy and accuracy. */
+
+  // While the encoders have not yet met their goal: (left is compared negativly since it will in reverse)
+  while(SensorValue[rightEncoder] < (r * rotations) && SensorValue[leftEncoder] > (-1 * r * rotations))
+  {
+    motor[rightMotor] = 63;         // Run the right motor forward at half speed
+    motor[leftMotor]  = -63;        // Run the left motor backward at half speed
+  }
+  motor[rightMotor] = 0;            /* Stop both motors!  This is important so that each function          */
+  motor[leftMotor]  = 0;            /* can act independantly as a "chunk" of code, without any loose ends. */
+}
+
+void TurnRight(float r)
+{
+  SensorValue[rightEncoder] = 0;    /* Clear the encoders for    */
+  SensorValue[leftEncoder]  = 0;    /* consistancy and accuracy. */
+
+  // While the encoders have not yet met their goal: (left is compared negativly since it will in reverse)
+  while(SensorValue[leftEncoder] < (r * rotations) && SensorValue[rightEncoder] > (-1 * r * rotations))
+  {
+    motor[rightMotor] = -63;         // Run the right motor forward at half speed
+    motor[leftMotor]  = 63;        // Run the left motor backward at half speed
   }
   motor[rightMotor] = 0;            /* Stop both motors!  This is important so that each function          */
   motor[leftMotor]  = 0;            /* can act independantly as a "chunk" of code, without any loose ends. */
